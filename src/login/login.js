db6198636d23107adf8ff8ab08c8bd39aadf62ab
Roberto <robertoleao1212@gmail.com>
2023-08-30 19:17:19 -0300

    correção de bugs na parte do if else

--- src/login/login.js
+++ src/login/login.js
@@ -4,21 +4,31 @@ const btnLogin = document.getElementById('btn-entrar')
 
 const erroUser = document.getElementById('usuario-empty')
 const erroPass = document.getElementById('senha-empty')
+var usuario = false
+var senha = false
 
 btnLogin.addEventListener('click', () => {
-    if (inputUser.value === "" && inputPass.value !== "") {
-        erroUser.removeChild(erroUser.lastChild);
+    // var verificarUsuario = document.getElementById('mensagemErro');
+    // console.log(verificarUsuario)
+   
+    // const erroUsuarioExistente = verificar(verificarUsuario)
+    // erroUsuarioExistente;
+    verificar()
+    if (inputUser.value === "" && inputPass.value !== "" && usuario === false) {
         let userVazio = document.createElement('p');
+        usuario = true
         userVazio.innerHTML = `<span style="color: red;"> Digite um usuário </span>`;
+        userVazio.id = "userID"
         erroUser.appendChild(userVazio);
-        erroPass.removeChild(erroPass.lastChild);
-    } else if (inputPass.value === "" && inputUser.value !== "") {
-    // mechemdo aqui erroPass.removeChild(erroPass.lastChild);
+    } else if (inputPass.value === "" && inputUser.value !== "" && senha === false) {
         let senhaVazio = document.createElement('p');
-        senhaVazio.innerHTML = `<span style="color: red;"> Digite uma senha</span>`;
+        senha = true
+        senhaVazio.id = "senhaID"
+        erroPass.innerHTML = `<span style="color: red;"> Digite uma senha </span>`;
         erroPass.appendChild(senhaVazio);
-        erroUser.removeChild(erroUser.lastChild);
     } else if (inputUser.value === "" && inputPass.value === "") {
+
+        // estou mexendo aqui
         let senhaVazio = document.createElement('p');
         let userVazio = document.createElement('p');
         senhaVazio.innerHTML = `<span style="color: red;"> Digite uma senha</span>`;
@@ -26,9 +36,21 @@ btnLogin.addEventListener('click', () => {
         erroPass.appendChild(senhaVazio);
         erroUser.appendChild(userVazio);
     } else {
-        validarDados()
+        console.log('sucesso ao logar')
     }
 })
+
+const verificar = () =>{
+    if(usuario === true){
+        erroUser.removeChild(erroUser.firstChild);
+        usuario = false
+    }
+    if(senha === true){
+        erroPass.removeChild(erroPass.firstChild);
+        senha = false
+    }
+}
+
 
 const validarDados = () => {
     //parei aqui
