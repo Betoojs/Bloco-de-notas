c44dc0c8d3652ec659cf1252fc4a4b43c63b7578
Roberto <robertoleao1212@gmail.com>
2023-09-02 14:01:44 -0300

    pagina principal

---
+++ src/pagina principal/index.js
@@ -0,0 +1,55 @@
+const carrinhoCompras = document.getElementById('menu');
+const btnAdd = document.getElementById('adicionar');
+const btnRem = document.getElementById('remover');
+const inputItem = document.getElementById('input');
+
+
+
+// adicionar item a lista
+
+btnAdd.addEventListener('click', () => {
+   if(inputItem.value === ""){
+    return alert("não tem nada para ser adicionado")
+    } 
+
+    var verificar = document.createElement('img');
+    verificar.src = './img/verificar.png'; // Defina o caminho correto da imagem
+    var verificado = document.createElement('img');
+    verificado.src = './img/verificado.png'; // Defina o caminho correto da imagem
+
+    var item = document.createElement('li');
+    item.innerHTML = `
+        <img id="img-verificar" src="${verificar.src}"/>
+        <img id="img-verificado" style="display: none;" src="${verificado.src}" />
+        ${inputItem.value}`;
+    carrinhoCompras.appendChild(item);
+    inputItem.value = '';
+
+    const imgVerificar = item.querySelector('#img-verificar');
+    const imgVerificado = item.querySelector('#img-verificado');
+
+    imgVerificar.addEventListener('click', () => {
+        imgVerificar.style.display = 'none';
+        imgVerificado.style.display = 'inline';
+    });
+
+    imgVerificado.addEventListener('click', () => {
+        imgVerificado.style.display = 'none';
+        imgVerificar.style.display = 'inline';
+    });
+});
+
+
+
+// remover item da lista
+
+btnRem.addEventListener('click', () => {
+    if (carrinhoCompras.lastChild) {
+        carrinhoCompras.removeChild(carrinhoCompras.lastChild);
+    } else if(inputItem.value === ""){
+        alert("não tem nada para ser removido")
+    }
+});
+
+
+// editar item da lista
